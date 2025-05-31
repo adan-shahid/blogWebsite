9bb9cc7102520b6333507d7f3315f782387ad52e
Adan <adanshahid.student@gmail.com>
2025-05-31 11:17:52 +0500

    Created blog app, added the templates and configured the views.py file

--- blogwebsite_main/urls.py
+++ blogwebsite_main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+
+
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
+    path("", blog.views.index, name="index"),
 ]
