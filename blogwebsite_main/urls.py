120bf418d02c26249ad98adbf69fed20ea9eafd0
Adan <adanshahid.student@gmail.com>
2025-06-01 10:31:26 +0500

    starting to implement the templates to render the models data. Facing an issue here

--- blogwebsite_main/urls.py
+++ blogwebsite_main/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', views.home, name='home'),
     path("", blog.views.index, name="index"),
+    path("index", blog.views.post_detail, name="blog-post-detail")
 ]
